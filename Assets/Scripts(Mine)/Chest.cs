6e96f3c576ddba209be7fbbea5b53206c9e7e028
Seth <sethunity@gmail.com>
2014-07-19 12:24:09 -0300

    Working on new tiles, added Chests

---
+++ Assets/Scripts(Mine)/Chest.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Scurge;
+using Scurge.Environment;
+using Scurge.Player;
+using Scurge.Util;
+using Scurge.Enemy;
+using Scurge.Audio;
+using Scurge.AI;
+using Scurge.Scoreboard;
+
+namespace Scurge.Environment {
+	public class Chest : MonoBehaviour {
+
+		public Inventory Inventory;
+		public AudioSource LootSound;
+
+		public GUISkin Skin;
+		public bool LookingIn = false;
+		public bool CanHave = true;
+
+		public Item item;
+
+		void Start() {
+			item = (Item)Random.Range(1, ItemDescription.Count);
+		}
+		void OnTriggerEnter(Collider collider) {
+			if(collider.gameObject.tag == "Player") {
+				LookingIn = true;
+			}
+		}
+		void OnTriggerExit(Collider collider) {
+			if(collider.gameObject.tag == "Player") {
+				LookingIn = false;
+			}
+		}
+		void OnGUI() {
+			GUI.skin = Skin;
+			if(LookingIn && CanHave) {
+				Inventory.Give(item, Inventory.FirstOpenSlot, InventoryBar.Inventory);
+				item = Item.None;
+				LootSound.Play();
+				CanHave = false;
+			}
+		}
+	}
+}
