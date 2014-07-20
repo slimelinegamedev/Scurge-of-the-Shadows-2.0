27c306edebe71a2675572afa098e0bb784d00f99
Seth <sethunity@gmail.com>
2014-07-20 17:46:38 -0300

    Working on multiplayer

---
+++ Assets/Scripts(Mine)/Multiplayer/Server.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Scurge;
+using Scurge.Environment;
+using Scurge.Player;
+using Scurge.Util;
+using Scurge.Enemy;
+using Scurge.Audio;
+using Scurge.AI;
+using Scurge.Scoreboard;
+
+namespace Scurge.Networking {
+	public class Server : MonoBehaviour {
+
+
+
+		//Unity
+		//Networking
+		//Custom
+	}
+}
